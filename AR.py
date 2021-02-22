438548cc921c05d51a7903c4952a7c0e307b7921
Easton Potokar <contagon6@gmail.com>
2021-02-22 13:42:29 -0700

    Added code to load different model for each marker

--- AR.py
+++ AR.py
@@ -2,39 +2,47 @@
 """
 Created on Fri Feb 19 16:42:57 2021
 
-@author: devon
+@author: devon, easton
 """
 
 import cv2
 import numpy as np
 from objloader_simple import *
-import os
-from tqdm import tqdm
 
 def render(img, obj, r, t, mtx, dist, color=False):
     vertices = obj.vertices
     # offset to middle of card
     h, w = 0, 0
 
-    for face in tqdm(obj.faces):
+    for face in (obj.faces):
+        # get points from obj file, shift them as needed
         face_vertices = face[0]
         points = np.array([vertices[vertex - 1] for vertex in face_vertices])
-
         points = np.array([[p[0] + w / 2, p[1] + h / 2, p[2]] for p in points])
 
+        # project them onto the image
         imgpts, jac = cv2.projectPoints(points, r, t, mtx, dist)
         imgpts = np.int32(imgpts)
 
+        # fill each polygon
         if color is False:
             cv2.fillConvexPoly(img, imgpts, (137, 27, 211))
         else:
-            color = hex_to_rgb(face[-1])
-            color = color[::-1] # reverse
+            print(color)
             cv2.fillConvexPoly(img, imgpts, color)
 
     return img
 
-obj = OBJ('models/squirtle.obj', swapyz=False)  
+# load all models and set color of each
+objs = [OBJ('models/squirtle.obj'),
+        OBJ('models/pikachu.obj'),
+        OBJ('models/charmander.obj'),
+        OBJ('models/bulbasaur.obj')]
+colors = [(91,163,181),
+            (244,220,38),
+            (229,56,0),
+            (91,143,99)]
+colors = [(c[2], c[1], c[0]) for c in colors]
 
 #load the predefined dictionary
 dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
@@ -50,18 +58,18 @@ cap = cv2.VideoCapture(2)
 while True: 
     #obtain camera image
     ret0, img = cap.read()                                                                                                                          
-    # img = cv2.imread('test_img.jpg')
         
     #detect the markers in the image
     markerCorners, markerIds, rejecteCandidates = cv2.aruco.detectMarkers(img, dictionary, parameters=parameters)
 
     if markerIds is not None:
-        for corners, id in zip(markerCorners, markerIds):
+        for corners, id in zip(markerCorners, markerIds.flatten()):
+            # estimate pose for each marker
             r, t, _ = cv2.aruco.estimatePoseSingleMarkers(corners, 50, camera_matrix, dist_coeffs)
             r = cv2.Rodrigues(r)[0]
-            # cv2.aruco.drawAxis(img, camera_matrix, dist_coeffs, r, t, 0.4)
 
-            img = render(img, obj, r, t, camera_matrix, dist_coeffs)
+            # put stuff on image
+            img = render(img, objs[id], r, t, camera_matrix, dist_coeffs, color=colors[id])
     
     # Showing the original image and the new output image side by side
     cv2.imshow("AR using Aruco markers", img)
