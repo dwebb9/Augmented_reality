c83df5ed25e257baa4ab101ee68125e3aa72ad3e
Easton Potokar <contagon6@gmail.com>
2021-02-22 12:12:31 -0700

    Got first simple version working!

--- AR.py
+++ AR.py
@@ -7,124 +7,66 @@ Created on Fri Feb 19 16:42:57 2021
 
 import cv2
 import numpy as np
+from objloader_simple import *
+import os
+from tqdm import tqdm
+
+def render(img, obj, r, t, mtx, dist, color=False):
+    vertices = obj.vertices
+    # offset to middle of card
+    h, w = 0, 0
+
+    for face in tqdm(obj.faces):
+        face_vertices = face[0]
+        points = np.array([vertices[vertex - 1] for vertex in face_vertices])
+
+        points = np.array([[p[0] + w / 2, p[1] + h / 2, p[2]] for p in points])
+
+        imgpts, jac = cv2.projectPoints(points, r, t, mtx, dist)
+        imgpts = np.int32(imgpts)
+
+        if color is False:
+            cv2.fillConvexPoly(img, imgpts, (137, 27, 211))
+        else:
+            color = hex_to_rgb(face[-1])
+            color = color[::-1] # reverse
+            cv2.fillConvexPoly(img, imgpts, color)
+
+    return img
+
+obj = OBJ('models/fox.obj', swapyz=True)  
 
 #load the predefined dictionary
 dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
+parameters = cv2.aruco.DetectorParameters_create()
 
-# Generate the markers
-n = 4
+# load params
+params = np.load('models/logitech_c270.npz')
+camera_matrix = params['mtx']
+dist_coeffs = params['dist']
 
-temp = np.zeros((200,200), dtype=np.uint8)
-markerImage = [temp]*n
-
-for i in range(0,n):
-    picture_number = i
-    temp = np.zeros((200,200), dtype=np.uint8)
-    markerImage[i] = cv2.aruco.drawMarker(dictionary,picture_number,200,temp,1)
-
-# #print images
-# cv2.imshow('marker 1', markerImage[0])
-# cv2.imshow('marker 2', markerImage[1])
-# cv2.imshow('marker 3', markerImage[2])
-# cv2.imshow('marker 4', markerImage[3])
-
-# #save images
-# cv2.imwrite("marker0.jpg", markerImage[0])
-# cv2.imwrite("marker1.jpg", markerImage[1])
-# cv2.imwrite("marker2.jpg", markerImage[2])
-# cv2.imwrite("marker3.jpg", markerImage[3])
-
-#import mask for test purposes
-mask = cv2.imread('test_img_mask.jpg')
-im_src = mask
-
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(2)
 
 while True: 
     #obtain camera image
-    
-    # # used for testing
-    # img = cv2.imread('test_img.jpg')
-    
     ret0, img = cap.read()                                                                                                                          
-    
-    # Initialize the detector parameters using default values
-    parameters = cv2.aruco.DetectorParameters_create()
-    
+    # img = cv2.imread('test_img.jpg')
+        
     #detect the markers in the image
     markerCorners, markerIds, rejecteCandidates = cv2.aruco.detectMarkers(img, dictionary, parameters=parameters)
-    
-    # put rectangles on markers. For testing purposes. 
-    # for i in range(0, n):
-    #     (x, y, w, h) = cv2.boundingRect(markerCorners[i])
-    #     cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    
-    im_out = img
-    
-    for k in range(0, 4):
-        #calculate Homography
-        index = np.squeeze(np.where(markerIds==k))
-        refPt1 = np.squeeze(markerCorners[index[0]])[2]
-        
-        index = np.squeeze(np.where(markerIds==k))
-        refPt2 = np.squeeze(markerCorners[index[0]])[3]
-        
-        distance = np.linalg.norm(refPt1-refPt2)
-        
-        scalingFac = 0.02
-        pts_dst = [[refPt1[0] - round(scalingFac*distance), refPt1[1] - round(scalingFac*distance)]]
-        pts_dst = pts_dst + [[refPt2[0] + round(scalingFac*distance), refPt2[1] - round(scalingFac*distance)]]
-        
-        index = np.squeeze(np.where(markerIds==k));
-        refPt3 = np.squeeze(markerCorners[index[0]])[0];
-        pts_dst = pts_dst + [[refPt3[0] + round(scalingFac*distance), refPt3[1] + round(scalingFac*distance)]];
-        
-        index = np.squeeze(np.where(markerIds==k));
-        refPt4 = np.squeeze(markerCorners[index[0]])[1];
-        pts_dst = pts_dst + [[refPt4[0] - round(scalingFac*distance), refPt4[1] + round(scalingFac*distance)]];
-        
-        pts_src = [[0,0], [im_src.shape[1], 0], [im_src.shape[1], im_src.shape[0]], [0, im_src.shape[0]]];
-        
-        pts_src_m = np.asarray(pts_src)
-        pts_dst_m = np.asarray(pts_dst)
-        
-        h, status = cv2.findHomography(pts_src_m, pts_dst_m)
-        
-        # Warp source image to destination based on homography
-        warped_image = cv2.warpPerspective(im_src, h, (img.shape[1],img.shape[0]))
-        
-        # Prepare a mask representing region to copy from the warped image into the original frame.
-        mask = np.zeros([img.shape[0], img.shape[1]], dtype=np.uint8);
-        cv2.fillConvexPoly(mask, np.int32([pts_dst_m]), (255, 255, 255), cv2.LINE_AA);
-        
-        # Erode the mask to not copy the boundary effects from the warping
-        element = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3));
-        mask = cv2.erode(mask, element, iterations=3);
-        
-        # Copy the mask into 3 channels.
-        warped_image = warped_image.astype(float)
-        mask3 = np.zeros_like(warped_image)
-        for i in range(0, 3):
-            mask3[:,:,i] = mask/255
-        
-        # Copy the warped image into the original frame in the mask region.
-        warped_image_masked = cv2.multiply(warped_image, mask3)
-        frame_masked = cv2.multiply(im_out.astype(float), 1-mask3)
-        im_out = cv2.add(warped_image_masked, frame_masked)
+
+    if markerIds is not None:
+        for corners, id in zip(markerCorners, markerIds):
+            r, t, _ = cv2.aruco.estimatePoseSingleMarkers(corners, 50, camera_matrix, dist_coeffs)
+            r = cv2.Rodrigues(r)[0]
+            # cv2.aruco.drawAxis(img, camera_matrix, dist_coeffs, r, t, 0.4)
+
+            img = render(img, obj, r, t, camera_matrix, dist_coeffs)
     
     # Showing the original image and the new output image side by side
-    concatenatedOutput = cv2.hconcat([img.astype(float), im_out]);
-    cv2.imshow("AR using Aruco markers", concatenatedOutput.astype(np.uint8))
-    
-    # cv2.imshow('img', im_src)
-    
-    # while True:
-    #     #print("got into while loop")
-    #     if cv2.waitKey(1) & 0xFF == ord('q'):
-    #         #print("read q")
-    #         break
-    
-    key = cv2.waitKey(100)
+    cv2.imshow("AR using Aruco markers", img)
+
+    key = cv2.waitKey(20)
     if key == ord('q'):
         break
 
