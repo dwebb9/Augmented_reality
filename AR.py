dab5f189518ecb62e50c211c1f04d156d29bf22c
Easton Potokar <contagon6@gmail.com>
2021-02-22 18:21:10 -0700

    Switched to different dictionary

--- AR.py
+++ AR.py
@@ -8,11 +8,26 @@ Created on Fri Feb 19 16:42:57 2021
 import cv2
 import numpy as np
 from objloader_simple import *
+import urllib.request 
+
+# clarify between small and big versions
+big = True
+if big:
+    size = 45
+    h, w = 0, -15
+else:
+    size = 45/3
+    h, w = 25, -15+50
+
+
+def save_markers(dictionary, n):
+    for i in range(n):
+        img = cv2.aruco.drawMarker(dictionary, i, 200)
+        cv2.imwrite(f'markers/marker{i}.jpg', img)
 
 def render(img, obj, r, t, mtx, dist, color=False):
     vertices = obj.vertices
     # offset to middle of card
-    h, w = 0, 0
 
     for face in (obj.faces):
         # get points from obj file, shift them as needed
@@ -28,7 +43,6 @@ def render(img, obj, r, t, mtx, dist, color=False):
         if color is False:
             cv2.fillConvexPoly(img, imgpts, (137, 27, 211))
         else:
-            print(color)
             cv2.fillConvexPoly(img, imgpts, color)
 
     return img
@@ -45,8 +59,14 @@ colors = [(91,163,181),
 colors = [(c[2], c[1], c[0]) for c in colors]
 
 #load the predefined dictionary
-dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
+dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
 parameters = cv2.aruco.DetectorParameters_create()
+parameters.adaptiveThreshWinSizeMax = 30
+parameters.adaptiveThreshWinSizeStep = 2
+parameters.adaptiveThreshConstant = 5
+
+# parameters.adaptiveThreshWinSizeMin = 2
+parameters.minDistanceToBorder = 0
 
 # load params
 params = np.load('params/logitech_c270.npz')
@@ -58,18 +78,18 @@ cap = cv2.VideoCapture(2)
 while True: 
     #obtain camera image
     ret0, img = cap.read()                                                                                                                          
-        
+
     #detect the markers in the image
     markerCorners, markerIds, rejecteCandidates = cv2.aruco.detectMarkers(img, dictionary, parameters=parameters)
 
     if markerIds is not None:
         for corners, id in zip(markerCorners, markerIds.flatten()):
-            # estimate pose for each marker
-            r, t, _ = cv2.aruco.estimatePoseSingleMarkers(corners, 50, camera_matrix, dist_coeffs)
-            r = cv2.Rodrigues(r)[0]
+            if id < 4:
+                # estimate pose for each marker
+                r, t, _ = cv2.aruco.estimatePoseSingleMarkers(corners, size, camera_matrix, dist_coeffs)
 
-            # put stuff on image
-            img = render(img, objs[id], r, t, camera_matrix, dist_coeffs, color=colors[id])
+                # put stuff on image
+                img = render(img, objs[id], r, t, camera_matrix, dist_coeffs, color=colors[id])
     
     # Showing the original image and the new output image side by side
     cv2.imshow("AR using Aruco markers", img)
